05bd482710fb22f027c1538b2ad72bdb0cbc2b88
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-11 17:22:17 +0530

    Add files via upload

---
+++ URL.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.net.*;
+public class Main
+{
+	public static void main(String[] args) throws Exception{
+		URL url=new URL(URL address);
+		System.out.println(url.getProtocol());
+		System.out.println(url.getHost());
+		System.out.println(url.getPort());
+		System.out.println(url.getFile());
+	}
+}
